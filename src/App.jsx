8de475d469534a959e379561d3298afbd55c29cd
Jamie Chan <jamiechan@Jamies-MacBook-Pro.local>
2024-01-07 21:38:21 -0800

    feat(app): display detail information of a single puppy

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,33 @@ import { puppyList } from "./data.js";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
 
   return (
     <div>
       {puppies.map((puppy) => {
-        return <p>{puppy.name}</p>;
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+          >
+            {puppy.name}
+          </p>
+        );
       })}
+      <div>
+        {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
